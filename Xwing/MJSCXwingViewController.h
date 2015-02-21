af9e635c9ee199ebfb71662024e3cff9c1577b27
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2015-02-21 09:59:23 +0100

    Reconocer gestos

---
+++ Xwing/MJSCXwingViewController.h
@@ -0,0 +1,14 @@
+//
+//  MJSCXwingViewController.h
+//  Xwing
+//
+//  Created by María Jesús Senosiain Caamiña on 20/02/15.
+//  Copyright (c) 2015 María Jesús Senosiain Caamiña. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJSCXwingViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *xwingView;
+
+@end
---
+++ Xwing/MJSCXwingViewController.m
@@ -0,0 +1,51 @@
+//
+//  MJSCXwingViewController.m
+//  Xwing
+//
+//  Created by María Jesús Senosiain Caamiña on 20/02/15.
+//  Copyright (c) 2015 María Jesús Senosiain Caamiña. All rights reserved.
+//
+
+#import "MJSCXwingViewController.h"
+
+@interface MJSCXwingViewController ()
+
+@end
+
+@implementation MJSCXwingViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    
+    // Creamos el gesture recognizer
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(userDidTap:)];
+    
+    // se lo encasquetamos a la vista
+    [self.view addGestureRecognizer:tap];
+}
+
+-(void) userDidTap:(UITapGestureRecognizer*) tap {
+    
+    // Mirar el estado del reconocedor: si lo ha reconocido
+    
+    if (tap.state == UIGestureRecognizerStateRecognized) {
+        // cambiamos la posicion de la vista del xwing
+        
+        self.xwingView.center = [tap locationInView:self.view];
+        
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+
+@end
