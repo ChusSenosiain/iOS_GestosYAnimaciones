837f28c7e663714746d45b7682f9ad7ee13fad36
María Jesús Senosiain Caamiña <mjsenosiainc@gmail.com>
2015-02-21 10:41:22 +0100

    - Compound animation: movement + rotation

--- Xwing/MJSCXwingViewController.m
+++ Xwing/MJSCXwingViewController.m
@@ -31,17 +31,31 @@
     // Mirar el estado del reconocedor: si lo ha reconocido
     
     if (tap.state == UIGestureRecognizerStateRecognized) {
-        // cambiamos la posicion de la vista del xwing
         
+        // Gestos: cambiamos la posicion de la vista del xwing
         //self.xwingView.center = [tap locationInView:self.view];
         
         
+        // Animaciones
         UIViewAnimationOptions option = UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut;
         
         
         [UIView animateWithDuration:1.2 delay:0 options:option animations:^{
             self.xwingView.center = [tap locationInView:self.view];
+            
         } completion:^(BOOL finished) {
+            
+        }];
+        
+        
+        [UIView animateWithDuration:0.6 delay:0 options:option animations:^{
+            self.xwingView.transform = CGAffineTransformMakeRotation(M_PI_2);
+        } completion:^(BOOL finished) {
+             // Con esto le devolvemos a su posición original
+            [UIView animateWithDuration:0.6 delay:0 options:option animations:^{
+                self.xwingView.transform = CGAffineTransformIdentity;
+            } completion:nil];
+           
             
         }];
         
